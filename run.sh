d64f4d21e3a50178f8f4b7c890eaf56f43538d5b
Ezra Reich <ezra@reich.be>
2023-01-18 11:54:28 +0000

    fix: complete bash script

--- run.sh
+++ run.sh
@@ -1,21 +1,33 @@
 #!/bin/sh 
-if [ $# -lt 2 ] ; then
-    if [ "$1" == "run"]; then 
-    echo 'Launching localhost server';
-    # cd israsoc_website 
-    # python manage.py runserver
-    else
-    echo ""
-    fi 
+#!/usr/bin/python3
 
-    # elif [ "$1" == "venv"]; then 
-    # echo 'Activate virtual environment';
-    # ./israsoc_venv/Scripts/activate 
-    # else
-    # echo ""
-    # fi
+if [ $# -lt 2 ] ; then
+    if [ "$1" = "run" ]; then 
+        echo 'Launching localhost server';
+        cd israsoc_website 
+        python manage.py runserver
+    elif [ "$1" = "venv" ]; then 
+        echo 'Activating virtual environment';
+        ./israsoc_venv/Scripts/activate 
+    elif [ "$1" = "deactivate" ] ; then 
+        echo 'Deactivating venv'; 
+        deactivate 
+    elif [ "$1" = "migration" ]; then 
+        echo 'Making migrations ... ';
+        cd israsoc_website
+        python manage.py makemigrations
+    elif [ "$1" = "migrate" ]; then 
+        echo 'Migrate dbsqlite ';
+        cd israsoc_website
+        python manage.py migrate
+    elif [ "$1" = "superuser" ]; then 
+        echo 'Creating SuperUser';
+        cd israsoc_website
+        python manage.py createsuperuser
     else
-    echo ""
+        echo 'Please enter a valid argument'
+    fi
 else
-    echo 'Please enter a valid argument'
+    echo "Too many arguments provided"
 fi
+
